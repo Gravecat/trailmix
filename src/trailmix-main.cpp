526524c9bc84a51c4fcc3b15a1f75c408dcd38a5
Raine 'Gravecat' Simmons <gc@gravecat.com>
2025-11-27 20:11:10 +0000

    Adjusted CMakeLists to build Trailmix as a static library.

--- src/trailmix-main.cpp
+++
@@ -1,7 +0,0 @@
-// trailmix-main.cpp -- Just a default blank main file, so the code can run automated build tests.
-
-// SPDX-FileType: SOURCE
-// SPDX-FileCopyrightText: Copyright 2025 Raine Simmons <gc@gravecat.com>
-// SPDX-License-Identifier: MIT
-
-int main() { return 0; }
