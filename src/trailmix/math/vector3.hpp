489a93d09f5e0d411beac7029d251ddfb63b9089
Raine 'Gravecat' Simmons <gc@gravecat.com>
2025-11-28 18:24:30 +0000

    Added a Vector3u class, with the same implicit/explicit conversions with Vector3 as Vector2/Vector2u.

--- src/trailmix/math/vector3.hpp
+++ src/trailmix/math/vector3.hpp
@@ -14,6 +14,8 @@
 
 namespace trailmix::math {
 
+struct Vector3u;    // Forward declaration, for explicit conversion of Vector3 to Vector3u.
+
 // Simple three-dimensional integer coordinate struct.
 struct Vector3
 {
@@ -43,7 +45,44 @@ struct Vector3
     template<typename T> Vector3    operator*(const T other) const { return {x * other, y * other, z * other}; }
     template<typename T> Vector3    operator%(const T other) const { return {x % other, y % other, z % other}; }
 
+    explicit operator Vector3u() const noexcept;    // Allows Vector3 to static_cast into Vector3 (beware of signed -> unsigned integer conversions)
+
     int32_t x, y, z;
+};
+
+// As above, but for explicitly unsigned integer vectors.
+struct Vector3u
+{
+    Vector3u() : x(0), y(0), z(0) { }
+    Vector3u(uint32_t vx, uint32_t vy, uint32_t vz) : x(vx), y(vy), z(vz) { }
+    Vector3u(std::initializer_list<uint32_t> list)
+    {
+        auto it = list.begin();
+        x = (it != list.end()) ? *it++ : 0;
+        y = (it != list.end()) ? *it++ : 0;
+        z = (it != list.end()) ? *it : 0;
+    }
+
+    Vector3u    operator+(const Vector3u &other) const { return { x + other.x, y + other.y, z + other.z }; }
+    Vector3u    operator-(const Vector3u &other) const { return { x - other.x, y - other.y, z - other.z }; }
+    Vector3u    operator/(const Vector3u &other) const { return { x / other.x, y / other.y, z / other.z }; }
+    Vector3u    operator*(const Vector3u &other) const { return { x * other.x, y * other.y, z * other.z }; }
+    Vector3u    operator%(const Vector3u &other) const { return { x % other.x, y % other.y, z % other.z }; }
+    bool        operator==(const Vector3u &other) const { return (x == other.x && y == other.y && z == other.z); }
+    bool        operator!=(const Vector3u &other) const { return (x != other.x || y != other.y || z != other.z); }
+    bool        operator!() const { return (x == 0 && y == 0 && z == 0); }
+    bool        operator>(const Vector3u &other) const { return (x > other.x && y > other.y && z > other.z); }
+    bool        operator<(const Vector3u &other) const { return (x < other.x || y < other.y || z < other.z); }
+    explicit    operator bool() const { return (x != 0 || y != 0 || z != 0); }
+
+    template<typename T> Vector3u   operator/(const T other) const { return {x / other, y / other, z / other}; }
+    template<typename T> Vector3u   operator*(const T other) const { return {x * other, y * other, z * other}; }
+    template<typename T> Vector3u   operator%(const T other) const { return {x % other, y % other, z % other}; }
+
+    // Allows implicit conversion of Vector3u into Vector3 (beware of unsigned -> signed integer conversions)
+    operator Vector3() const noexcept { return { static_cast<int32_t>(x), static_cast<int32_t>(y) }; }
+
+    uint32_t x, y, z;
 };
 
 }   // namespace trailmix::math
