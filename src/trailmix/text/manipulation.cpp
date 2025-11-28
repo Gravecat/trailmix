a94e4e690e7fd7911d18b12aeb7297e7a56f18f5
Raine 'Gravecat' Simmons <gc@gravecat.com>
2025-11-28 03:36:22 +0000

    Added text::manipulation::possessive_string()

--- src/trailmix/text/manipulation.cpp
+++ src/trailmix/text/manipulation.cpp
@@ -62,6 +62,14 @@ bool find_and_replace(string& input, const string& to_find, const string& to_rep
 string join_words(vector<string> vec, const string& spacer)
 { return std::accumulate(vec.begin() + 1, vec.end(), vec[0], [&spacer](const std::string& a, const std::string& b) { return a + spacer + b; }); }
 
+// Makes a string into a possessive noun (e.g. orc = orc's, platypus = platypus')
+string possessive_string(const string& str)
+{
+    if (str.empty()) return "";
+    if (str[str.size() - 1] == 's' || str[str.size() - 1] == 'S') return str + "'";
+    else return str + "'s";
+}
+
 // String split/explode function.
 vector<string> string_explode(string str, const string& separator)
 {
--- src/trailmix/text/manipulation.hpp
+++ src/trailmix/text/manipulation.hpp
@@ -15,6 +15,7 @@ namespace trailmix::text::manipulation {
 std::string decode_compressed_string(std::string cb);   // Decodes a compressed string (e.g. 4cab2z becomes ccccabzz).
 bool        find_and_replace(std::string& input, const std::string& to_find, const std::string& to_replace);    // Find and replace one string with another.
 std::string join_words(std::vector<std::string> vec, const std::string& spacer = " ");  // Takes a vector of strings and squashes them into one string.
+std::string possessive_string(const std::string& str);  // Makes a string into a possessive noun (e.g. orc = orc's, platypus = platypus')
 std::vector<std::string>    string_explode(std::string str, const std::string& separator = " ");    // String split/explode function.
 
 }   // namespace trailmix::text::manipulation
